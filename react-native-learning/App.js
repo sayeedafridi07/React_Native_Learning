0db79545c6385df2666d51e140f268f76af84e1b
sayeedafridi07 <sayeedafridi07@gmail.com>
2024-03-29 01:48:33 +0530

    Styling in RN

--- react-native-learning/App.js
+++ react-native-learning/App.js
@@ -4,10 +4,18 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>Another piece of text!</Text>
+        <Text
+          style={styles.dummyText}
+        >
+          Another piece of text!
+        </Text>
       </View>
-      <Text>Hello World!</Text>
-      <Button title='Tap me!' />
+      <Text
+        style={styles.dummyText}
+      >
+        Hello World!
+      </Text>
+      <Button title="Tap me!" />
     </View>
   );
 }
@@ -19,4 +27,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: 'blue',
+  }
 });
